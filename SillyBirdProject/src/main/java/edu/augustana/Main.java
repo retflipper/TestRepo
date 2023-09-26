f05396ecfbe326e1e9ae2a85b6c61d2e28a8b7b1
Logan <footballlfar@gmail.com>
2023-09-26 13:30:40 -0500

    "Now with dancing chicken"

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,10 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello bird world!");
+
+        Chicken nuggets = new Chicken();
+        nuggets.dance();
     }
+
 }
